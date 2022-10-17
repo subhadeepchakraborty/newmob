b504ae4145cceedb05052e69fdeec7c2482f0559
subhadeepchakraborty <33681479+subhadeepchakraborty@users.noreply.github.com>
2022-10-17 20:20:17 +0530

    Update apiFile.php

--- apiFile.php
+++ apiFile.php
@@ -9,8 +9,8 @@ $dbname = "sql6525693";
 $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
-$ok =mail("mobwebguru@gmail.com","Query Website","heelo");
-
+$ok =mail("csubh143@gmail.com","Query Website","heelo");
+echo .$ok;
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
@@ -21,7 +21,7 @@ if ($conn->connect_error) {
 $sql = "INSERT INTO userTable (name, email, message) VALUES ('$name', '$email', '$message')";
 
 if ($conn->query($sql) === TRUE) {
-  echo "we will contact you soon.".$ok;
+  echo "we will contact you soon.";
 } else {
   echo "Error: " . $sql . "<br>" . $conn->error;
 }
